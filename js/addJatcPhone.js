aba0bb576be5c0cfaace42c6881559603db35896
Cana Owens <canaowens@gmail.com>
2021-10-28 11:37:08 -0400

    Update addJatcPhone.js
    Reworked the addJatcPhone function to use the insertAdjecentHTML() method and window.matchMedia() to add the phone number if the screen is smaller than 800px

--- js/addJatcPhone.js
+++ js/addJatcPhone.js
@@ -8,29 +8,19 @@
 
  const addPhoneNumberOnSmallScreen = {
 
-    missingJatcPhoneNumber : document.querySelectorAll(".jatc-phone"),
-
     init : function() {
-
-
-        this.addJatcPhoneOnSmallScreen();
-
+        if (window.matchMedia('(max-width: 800px)').matches) {
+            this.addJatcPhoneOnSmallScreen();
+        }
     },
 
-    
-
     addJatcPhoneOnSmallScreen: function() {
-        let jatcPhone = addPhoneNumberOnSmallScreen.missingJatcPhoneNumber;
-        let jatcPhoneArray = Array.from(addPhoneNumberOnSmallScreen.missingJatcPhoneNumber);
-        console.log(jatcPhone);
+        let jatcPhone = document.querySelectorAll('.jatc-phone');
+        let jatcPhoneArray = Array.from(jatcPhone);
 
-        let newDataCell = document.createElement('td');
-        newDataCell.textContent = "800-638-0592";
-
-        jatcPhoneArray.forEach(item => {
-            item.parentNode.insertBefore(newDataCell, item.nextSibling)
-
-        }) 
+       jatcPhoneArray.forEach( (item) => {
+           item.insertAdjacentHTML('afterend', "<td>800-638-0592</td>");
+       });
             
     }
 
