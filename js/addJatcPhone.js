dafae035ca30f9ea4438353a51ab207bbbe01aed
Cana Owens <canaowens@gmail.com>
2021-10-29 10:58:55 -0400

    Update addJatcPhone.js
    added function documentation

--- js/addJatcPhone.js
+++ js/addJatcPhone.js
@@ -1,8 +1,6 @@
 /**
- * 
- * 
- * 
- * 
+ * @function init - where the code starts & checks for the window size based on the matching media query
+ * @function addJatcPhoneOnSmallScreen - prepends the JATC phone number if the screen is smaller than 900px to all data cells with the class of "jatc-phone". The screen size matches the media query in the table-styles.css file.
  * 
  * */ 
 
