b5a23fe4205c3b718acd625575e1e67daf85c9bc
Cana Owens <canaowens@gmail.com>
2022-01-27 11:03:20 -0500

    Update addJatcPhone.js
    Added an event listener to check for if the element exists. If it does, then don't do anything. If the window size is bigger, then delete the element.

--- js/addJatcPhone.js
+++ js/addJatcPhone.js
@@ -1,15 +1,22 @@
 /**
- * @function init - where the code starts & checks for the window size based on the matching media query
+ * @function init - where the code starts & checks for the window size based on the matching media query and listens for when the window is being resized.
  * @function addJatcPhoneOnSmallScreen - prepends the JATC phone number if the screen is smaller than 900px to all data cells with the class of "jatc-phone". The screen size matches the media query in the table-styles.css file.
+ * @function removeJatcPhoneOnSmallScreen - removes the added <td> with the JATC phone number on screens larger than 900px.
  * 
  * */ 
 
  const addPhoneNumberOnSmallScreen = {
 
     init : function() {
-        if (window.matchMedia('(max-width: 900px)').matches) {
-            this.addJatcPhoneOnSmallScreen();
-        }
+        window.addEventListener('resize', function() {
+            let jatcPhoneAdded = document.querySelector('td.jatc-phone-added');
+            
+            if(!jatcPhoneAdded && window.matchMedia('(max-width: 900px)').matches) {
+                addPhoneNumberOnSmallScreen.addJatcPhoneOnSmallScreen();
+            } else if (jatcPhoneAdded && window.matchMedia('(min-width: 900px)').matches) {
+                addPhoneNumberOnSmallScreen.removeJatcPhoneOnSmallScreen();
+            }
+        });
     },
 
     addJatcPhoneOnSmallScreen: function() {
@@ -17,10 +24,16 @@
         let jatcPhoneArray = Array.from(jatcPhone);
 
        jatcPhoneArray.forEach( (item) => {
-           item.insertAdjacentHTML('afterend', "<td>800-638-0592</td>");
+           item.insertAdjacentHTML('afterend', '<td class="jatc-phone-added">800-638-0592</td>');
        });
-            
-    }
+    },
+
+    removeJatcPhoneOnSmallScreen: function() {
+        let jatcPhone = document.querySelectorAll('.jatc-phone-added');
+        jatcPhone.forEach( (item) => {
+            item.remove();
+        })
+    },
 
 
 }
